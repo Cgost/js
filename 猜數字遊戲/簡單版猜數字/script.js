d3f037f1947e47492864070c4d614b2dddefc426
DESKTOP-335ND2V\陳柏諭 <564732.eee27>
2023-11-16 16:27:06 +0800

    1. 將 "幾A幾B" 修改為 "猜數字遊戲" 2. 新增簡單版猜數字遊戲 & 困難版猜數字(幾A幾B)

---
+++ 猜數字遊戲/簡單版猜數字/script.js
@@ -0,0 +1,48 @@
+var text = document.querySelector("#text");
+var count = document.querySelector("#count");
+var result = document.querySelector(".result");
+var guessBu = document.querySelector("#guess");
+var reBu = document.querySelector("#reBu");
+var resultDisplay = document.querySelector("#result");
+
+var minRange = 1;
+var maxRange = 100;
+
+var guessNumber = Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
+var sum = 0;
+
+guessBu.onclick = function () {
+    sum++;
+    count.innerHTML = sum;
+    var userGuess = parseInt(text.value);
+
+    if (userGuess > guessNumber) {
+        result.innerHTML = "猜大了！";
+        result.style.color = "red";
+        maxRange = userGuess;
+        resultDisplay.innerHTML = `${minRange}-${maxRange}`;
+    } else if (userGuess < guessNumber) {
+        result.innerHTML = "猜小了！";
+        result.style.color = "red";
+        minRange = userGuess;
+        resultDisplay.innerHTML = `${minRange}-${maxRange}`;
+    } else {
+        result.className = "c2";
+        result.innerHTML = "恭喜猜對了！";
+        result.style.color = "green";
+        resultDisplay.innerHTML = "恭喜猜對了！";
+    }
+}
+
+reBu.onclick = function () {
+    minRange = 1;
+    maxRange = 100;
+    guessNumber = Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
+    sum = 0;
+    count.innerHTML = sum;
+    result.innerHTML = "";
+    resultDisplay.innerHTML = "";
+    text.value = "";
+    // Reassign the result variable after clearing the resultDisplay
+    result = document.querySelector(".c1");
+}
